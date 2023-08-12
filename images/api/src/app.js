cb13fe0688ff78a0bfa19c94b01f85a9953fd1f8
AndyHuizenga <56120771+AndyHuizenga@users.noreply.github.com>
2023-08-12 23:10:31 +0200

    Docker_working
    Finished video one - docker is working correctly.

--- images/api/src/app.js
+++ images/api/src/app.js
@@ -5,9 +5,9 @@ app.get("/", (req, res) => {
   res.send({message: "Hello, World!"});
 })
 
-app.listen(5500, (err) => {
+app.listen(5502, (err) => {
   if (!err) {
-    console.log("Server is running at http://localhost:5500");
+    console.log("Server is running at http://localhost:5502");
     
   } else {
     console.error(err);
