e6ad11ead189b36c9b612b4f54454e71d53d1a79
AndyHuizenga <56120771+AndyHuizenga@users.noreply.github.com>
2023-08-17 16:10:57 +0200

    BOOTSTRAP
    Bootstrap added to navigation bar

--- frontend/osc-data-display/frontend/src/App.js
+++ frontend/osc-data-display/frontend/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 import OSCDataDisplay from './OSCDataDisplay';
 import PaintingCanvas from './PaintingCanvas';
 import OSCPaintingCanvas from './OSCPaintingCanvas';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 
 
 
@@ -11,18 +13,35 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">OSC Data Display</Link>
-            </li>
-            <li>
-              <Link to="/painting">Painting Canvas</Link>
-            </li>
-            <li>
-              <Link to="/oscpainting">OSC Painting Canvas</Link>
-            </li>
-          </ul>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <div className="container">
+            <Link className="navbar-brand" to="/">OSC ART</Link>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNav"
+              aria-controls="navbarNav"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <Link className="nav-link" to="/">OSC Data Display</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/painting">Painting Canvas</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/oscpainting">OSC Painting Canvas</Link>
+                </li>
+              </ul>
+          
+            </div>
+          </div>
         </nav>
 
         <Routes>
