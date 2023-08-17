39bdc2e1509de547c71e47570930cabecbaedf7c
AndyHuizenga <56120771+AndyHuizenga@users.noreply.github.com>
2023-08-17 11:36:24 +0200

    DRAWING_function
    Drawing function put in place, canvas visible on canvas page.

--- frontend/osc-data-display/frontend/src/PaintingCanvas.js
+++ frontend/osc-data-display/frontend/src/PaintingCanvas.js
@@ -1,10 +1,36 @@
-import React from 'react';
-import './PaintingCanvas.css'; // Import your local CSS file
+import React, { useState, useEffect, useRef } from 'react';
+import './PaintingCanvas.css'
 
 const PaintingCanvas = () => {
+  const [paintingWorks, setPaintingWorks] = useState([]);
+  const canvasRef = useRef(null);
+
+  const draw = (context, works) => {
+    context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clear the canvas
+
+    context.strokeStyle = 'blue'; // Set the stroke color
+    context.lineWidth = 2; // Set the line width
+
+    works.forEach((work) => {
+      context.beginPath();
+      context.moveTo(work.startX, work.startY);
+      context.lineTo(work.endX, work.endY);
+      context.stroke();
+    });
+  };
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const context = canvas.getContext('2d');
+
+    draw(context, paintingWorks);
+  }, [paintingWorks]);
+
   return (
     <div className="painting-canvas-container">
-      <div className="canvas"></div>
+      <div className="canvas">
+        <canvas ref={canvasRef}></canvas>
+      </div>
     </div>
   );
 };
