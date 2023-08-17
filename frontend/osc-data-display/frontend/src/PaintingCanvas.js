0eeb97ece9d9320994705c681602065cbd07c7cd
AndyHuizenga <56120771+AndyHuizenga@users.noreply.github.com>
2023-08-17 11:52:20 +0200

    PAINTING-working
    Painitng with my mouse is working with an EventListen

--- frontend/osc-data-display/frontend/src/PaintingCanvas.js
+++ frontend/osc-data-display/frontend/src/PaintingCanvas.js
@@ -1,35 +1,65 @@
-import React, { useState, useEffect, useRef } from 'react';
-import './PaintingCanvas.css'
+import React, { useState, useEffect } from 'react';
+import './PaintingCanvas.css';
 
 const PaintingCanvas = () => {
-  const [paintingWorks, setPaintingWorks] = useState([]);
-  const canvasRef = useRef(null);
-
-  const draw = (context, works) => {
-    context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clear the canvas
-
-    context.strokeStyle = 'blue'; // Set the stroke color
-    context.lineWidth = 2; // Set the line width
-
-    works.forEach((work) => {
-      context.beginPath();
-      context.moveTo(work.startX, work.startY);
-      context.lineTo(work.endX, work.endY);
-      context.stroke();
-    });
-  };
+  const [isPainting, setIsPainting] = useState(false);
+  const [strokeColor, setStrokeColor] = useState('#0000FF');
+  const [drawing, setDrawing] = useState([]);
 
   useEffect(() => {
-    const canvas = canvasRef.current;
-    const context = canvas.getContext('2d');
+    const handleMouseDown = (event) => {
+      const newDrawing = [...drawing];
+      newDrawing.push([]);
+      setDrawing(newDrawing);
+      setIsPainting(true);
+      handleMouseMove(event);
+    };
 
-    draw(context, paintingWorks);
-  }, [paintingWorks]);
+    const handleMouseMove = (event) => {
+      if (!isPainting) return;
+      const { clientX, clientY } = event;
+      const canvas = document.querySelector('.canvas');
+      const rect = canvas.getBoundingClientRect();
+      const x = clientX - rect.left;
+      const y = clientY - rect.top;
+
+      const updatedDrawing = [...drawing];
+      const lastLine = updatedDrawing[updatedDrawing.length - 1];
+      lastLine.push({ x, y });
+
+      setDrawing(updatedDrawing);
+    };
+
+    const handleMouseUp = () => {
+      setIsPainting(false);
+    };
+
+    const canvas = document.querySelector('.canvas');
+    canvas.addEventListener('mousedown', handleMouseDown);
+    canvas.addEventListener('mousemove', handleMouseMove);
+    canvas.addEventListener('mouseup', handleMouseUp);
+
+    return () => {
+      canvas.removeEventListener('mousedown', handleMouseDown);
+      canvas.removeEventListener('mousemove', handleMouseMove);
+      canvas.removeEventListener('mouseup', handleMouseUp);
+    };
+  }, [isPainting, drawing, strokeColor ]);
 
   return (
     <div className="painting-canvas-container">
       <div className="canvas">
-        <canvas ref={canvasRef}></canvas>
+        <svg className="drawing">
+          {drawing.map((line, index) => (
+            <polyline
+              key={index}
+              points={line.map(({ x, y }) => `${x},${y}`).join(' ')}
+              fill="none"
+              stroke={strokeColor} // Use the selected stroke color
+              strokeWidth="3"
+            />
+          ))}
+        </svg>
       </div>
     </div>
   );
