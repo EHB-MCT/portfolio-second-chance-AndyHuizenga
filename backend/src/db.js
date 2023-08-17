a46d5b905c4378733e1d95c739f3d0948c020804
AndyHuizenga <56120771+AndyHuizenga@users.noreply.github.com>
2023-08-17 08:53:19 +0200

    Nodemon-OSC

--- backend/src/db.js
+++ backend/src/db.js
@@ -2,11 +2,11 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  host: process.env.POSTGRES_IP, // Replace with PostgreSQL container's IP
-  port: 5432, // Default PostgreSQL port
-  database: 'postgres', // Default PostgreSQL database
-  user: 'postgres', // Default PostgreSQL username
-  password: process.env.POSTGRES_PASSWORD // Use the password you set in .env
+  host: process.env.DB_HOST, // Use the host from .env
+  port: process.env.DB_PORT, // Use the port from .env
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD
 });
 
 module.exports = pool;
