d4d001c94b04b910738be0320f34ea6ba32c2966
AndyHuizenga <56120771+AndyHuizenga@users.noreply.github.com>
2023-08-19 03:36:59 +0200

    REFRESH
    Table refresh after clearing OSC button

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const cors = require('cors');
-const pool = require('./db'); // Import the database connection
+const http = require('http');
+const socketIo = require('socket.io');
 const osc = require('osc');
-const http = require('http'); // Import the http module
-const socketIo = require('socket.io'); // Import socket.io
 require('dotenv').config();
 
 const app = express();
-const server = http.createServer(app); // Create a http server
+const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
     origin: "http://localhost:3000", // Adjust this to match your frontend URL
@@ -15,13 +14,8 @@ const io = socketIo(server, {
   }
 });
 
-// Use CORS middleware
 app.use(cors());
 
-// Test route to check the database connection
-
-
-// Server for OSC data
 const oscReceivedData = [];
 
 const oscServer = server.listen(3001, () => {
@@ -44,9 +38,7 @@ udpPort.on('message', (oscMsg) => {
     console.log('Received OSC message A:', transformedMessage);
 
     oscReceivedData.push(transformedMessage);
-
-    // Send the new data to connected socket.io clients
-    io.emit('osc-data-update', transformedMessage);
+    io.emit('osc-data-update', transformedMessage); // Emit the new data to all connected clients
   }
 });
 
@@ -55,10 +47,12 @@ udpPort.open();
 io.on('connection', (socket) => {
   console.log('A user connected');
   
-  // Socket event handling here
-  
-  // Send initial data to the connected client, if needed
-  socket.emit('initial-data', oscReceivedData);
+  // Send the initial OSC data to the connected client
+  socket.emit('initial-osc-data', oscReceivedData);
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
 });
 
 app.get('/oscdata', (req, res) => {
@@ -70,8 +64,6 @@ app.post('/cleardata', (req, res) => {
   res.json({ message: 'OSC data cleared' });
 });
 
-// Default route
 app.get('/', (req, res) => {
   res.send('Hello, this is the homepage of your application!');
 });
-
--- frontend/src/OSCDataDisplay.js
+++ frontend/src/OSCDataDisplay.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import './OSCDataDisplay.css'; // Import your local CSS file
+import './OSCDataDisplay.css';
 import { handleClearData } from './Buttons';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import io from 'socket.io-client'; // Import socket.io-client
 
 function OSCDataDisplay() {
   const [oscData, setOSCData] = useState([]);
@@ -15,35 +16,57 @@ function OSCDataDisplay() {
       .catch((error) => {
         console.error('Error fetching OSC data:', error);
       });
-  }, []);
+
+    const socketInstance = io('http://localhost:3001'); // Replace with your backend URL
+
+    socketInstance.on('osc-data-update', (newData) => {
+      setOSCData((prevData) => [...prevData, newData]);
+    });
+
+    return () => {
+      socketInstance.disconnect(); // Cleanup socket connection on unmount
+    };
+  }, []); // Empty dependency array means this effect runs only once on mount
+
+  const handleClearData = async () => {
+    try {
+      const response = await fetch('http://localhost:3001/cleardata', {
+        method: 'POST',
+      });
+
+      if (response.ok) {
+        setOSCData([]); // Clear the table by updating the state
+      }
+    } catch (error) {
+      console.error('Error clearing OSC data:', error);
+    }
+  };
 
   return (
     <div>
       <h1>OSC Data Display</h1>
-      
-      <button onClick={handleClearData} className="btn btn-danger ">
+
+      <button onClick={handleClearData} className="btn btn-danger">
         Clear OSC Data
       </button>
       <div className="tableS">
-      <table className="table table-bordered table-width-80" >
-      <thead className="thead-dark">
-          <tr>
-            <th>X-POS</th>
-            <th>Y-POS</th>
-          </tr>
-        </thead>
-        <tbody>
-          {oscData.map((data, index) => (
-            <tr key={index}>
-              <td>{data['X-POS']}</td>
-              <td>{data['Y-POS']}</td>
+        <table className="table table-bordered table-width-80">
+          <thead className="thead-dark">
+            <tr>
+              <th>X-POS</th>
+              <th>Y-POS</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-</div>
-
-      
+          </thead>
+          <tbody>
+            {oscData.map((data, index) => (
+              <tr key={index}>
+                <td>{data['X-POS']}</td>
+                <td>{data['Y-POS']}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
