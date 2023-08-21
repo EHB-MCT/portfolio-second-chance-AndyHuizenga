5d25bb8ab7e824f2859e9c9014b767712698d1c0
AndyHuizenga <56120771+AndyHuizenga@users.noreply.github.com>
2023-08-21 23:07:17 +0200

    SERVER ROUTE
    Clear descriptions of all server routes

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ require('dotenv').config();
 const pool = require('./db'); 
 
 
+
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
@@ -18,11 +19,17 @@ const io = socketIo(server, {
 
 app.use(cors());
 
+/**
+ * GET endpoint, filters the incoming object from the OSC port
+ *
+ * @param object incoming obect from the OSC port to be filtered and transformed into a new object (filterd info: devicetype, adress, running system)
+* @returns object oscMsg, containing X-POS(float) and Y-POS(float)
+ * 
+ */
+
 const oscReceivedData = [];
 
-const oscServer = server.listen(3001, () => {
-  console.log('OSC Server is running on port 3001');
-});
+
 
 const udpPort = new osc.UDPPort({
   localAddress: '0.0.0.0',
@@ -31,6 +38,7 @@ const udpPort = new osc.UDPPort({
 });
 
 udpPort.on('message', (oscMsg) => {
+  console.log('Received OSC message:', oscMsg);
   if (oscMsg.address === '/ZIGSIM/t_2x-NTvYcRN220d/touch0') {
     const transformedMessage = {
       "X-POS": oscMsg.args[0].value,
@@ -46,6 +54,10 @@ udpPort.on('message', (oscMsg) => {
 
 udpPort.open();
 
+/**
+ * Socket.IO setup and handles the connection and disconnection events of clients in the frontend
+ */
+
 io.on('connection', (socket) => {
   console.log('A user connected');
   
@@ -57,33 +69,61 @@ io.on('connection', (socket) => {
   });
 });
 
+
+/**
+ * GET endpoint, route to get the OSC data
+ *
+ * @param object route made available on /oscdata to get the OSC data
+ * @returns  contains array with all the OSC DATA : X-POS(float) and Y-POS(float)
+ * 
+ */
+// Endpoint to get OSC data
 app.get('/oscdata', (req, res) => {
   res.json(oscReceivedData);
-  res.json({ message: 'OSC_INCOMING' });
-
 });
 
+/**
+ * POST endpoint, route to clear the OSC data
+ *
+ * @param object route made available on /cleardata to clear the OSC data array oscReceivedData
+ * @returns empty array oscrReceivedData
+ * 
+ */
+// Endpoint to clear OSC data
 app.post('/cleardata', (req, res) => {
   oscReceivedData.length = 0; // Clear the oscReceivedData array
-
+  res.json({ message: 'OSC data cleared' });
 });
 
+/**
+ * GET endpoint,
+ *
+ * @param object route made available on / to get the check if backend is available
+ * @returns text message "Hello, this is the homepage of your application!"
+ * 
+ */
+// Homepage route
 app.get('/', (req, res) => {
   res.send('Hello, this is the homepage of your application!');
 });
 
+/**
+ * POST endpoint, route to save the drawing data
+ *
+ * @param object route made available on /savedata to save the drawing data
+ * @returns text message "Data inserted into the database", if data is inserted into the database
+ * 
+ */
 app.use(express.json()); // To parse JSON-encoded bodies
 app.use(express.urlencoded({ extended: true }));
+
+// Endpoint to save drawing data to the database
 app.post('/savedata', async (req, res) => {
   const receivedDrawing = req.body.drawing; 
-  console.log(receivedDrawing, "the whole is in the backend yet here");
 
-  console.log(receivedDrawing[0], "XXXX");
-  console.log(receivedDrawing.X_POSALL, "alone");
+  // Extract X_POSALL and Y_POSALL from the received data
   const X_POSALL = receivedDrawing.X_POSALL;
   const Y_POSALL = receivedDrawing.Y_POSALL;
-  console.log(X_POSALL, "X_POSALL");
-  console.log(Y_POSALL, "Y_POSALL");
 
   try {
     await pool.query(
@@ -96,7 +136,6 @@ app.post('/savedata', async (req, res) => {
     console.error('Error inserting data:', error);
     res.status(500).json({ error: 'An error occurred' });
   }
-
-
 });
+
 
